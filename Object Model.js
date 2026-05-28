74aec6442f1628ba6c1bc1d07e4daca4cb9a83fe
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-05-28 15:17:37 -0400

    project setup

--- Object Model.js
+++ Object Model.js
@@ -6,3 +6,26 @@ const playlistContainer = document.getElementById("playlist-container");
 const errorMessage = document.getElementById("error-message");
 const savedMoodText = document.getElementById("saved-mood-text");
 const songTemplate = document.getElementById("song-template");
+
+const playlists = {
+  chill: [
+    { title: "Show Me How", artist: "Men I Trust" },
+    { title: "Sweet Boy", artist: "Malcolm Todd" },
+    { title: "Reflections", artist: "The Neighbourhood" }
+  ],
+  sad: [
+    { title: "Cry", artist: "Cigarettes After Sex" },
+    { title: "Wildflower", artist: "Billie Eilish" },
+    { title: "No One Noticed", artist: "The Marías" }
+  ],
+  hype: [
+    { title: "Swim", artist: "Chase Atlantic" },
+    { title: "Into It", artist: "Chase Atlantic" },
+    { title: "The Prom", artist: "Glaive" }
+  ],
+  focus: [
+    { title: "Heavy", artist: "The Marías" },
+    { title: "Slow Down", artist: "Chase Atlantic" },
+    { title: "Nervous", artist: "The Neighbourhood" }
+  ]
+};
